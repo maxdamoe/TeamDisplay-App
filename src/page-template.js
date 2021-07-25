8433b045fb3c21005eb177f355e0496c9e0493dd
msimp <max.simpson@gmail.com>
2021-07-25 13:19:28 -0500

    experimentation, learning, really messy

---
+++ src/page-template.js
@@ -0,0 +1,121 @@
+
+// const generatePage = (data) => {
+//     return `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+//     <link rel="stylesheet" href="./Assets/style.css" />
+
+//     <title>Team project app</title>
+// </head>
+
+// <body>
+//     <div class="row">
+//         <div class="main">
+//             <div class="title">
+//                 <h1>{data.name}</h1>
+//                 <h2>   ${data.name}</h2>
+//             </div>
+//             <div class="identification">
+//                 <p>ID: 1</p>
+//             </div>
+//             <div class="email">
+//                 <p>Email: ${data.email}</p>
+//             </div>
+//             <div class="github">
+//                 <p>Github: ${data.github}</p>
+//             </div>
+//         </div>
+//         <div class="main">
+//             <div class="title">
+//                 <h1>{data.name}</h1>
+//                 <h2>   ${data.name}</h2>
+//             </div>
+//             <div class="identification">
+//                 <p>ID: 2</p>
+//             </div>
+//             <div class="email">
+//                 <p>Email: ${data.email}</p>
+//             </div>
+//             <div class="github">
+//                 <p>Github: ${data.github}</p>
+//             </div>
+//         </div>
+//         <div class="main">
+//             <div class="title">
+//                 <h1>${data.name}</h1>
+//                 <h2>   ${data.name}</h2>
+//             </div>
+//             <div class="identification">
+//                 <p>ID: 3</p>
+//             </div>
+//             <div class="email">
+//                 <p>Email: ${data.email}</p>
+//             </div>
+//             <div class="github">
+//                 <p>Github: ${data.github}</p>
+//             </div>
+//         </div>
+//     </div>
+//     <div class="row">
+//         <div class="main">
+//             <div class="title">
+//                 <h1>${data.name}</h1>
+//                 <h2>   ${data.name}</h2>
+//             </div>
+//             <div class="identification">
+//                 <p>ID: 4</p>
+//             </div>
+//             <div class="email">
+//                 <p>Email: ${data.email}</p>
+//             </div>
+//             <div class="github">
+//                 <p>Github: ${data.github}</p>
+//             </div>
+//         </div>
+//         <div class="main">
+//             <div class="title">
+//                 <h1>${data.name}</h1>
+//                 <h2>   ${data.name}</h2>
+//             </div>
+//             <div class="identification">
+//                 <p>ID: 5</p>
+//             </div>
+//             <div class="email">
+//                 <p>Email: ${data.email}</p>
+//             </div>
+//             <div class="github">
+//                 <p>Github: ${data.github}</p>
+//             </div>
+//         </div>
+//     </div>
+
+
+
+
+
+// </body>
+// </html>
+
+// `
+// }
+
+// // create the about section
+// // const generateAbout = aboutText => {
+// //     if (!aboutText) {
+// //       return '';
+// //     }
+  
+// //     return `
+// //       <section class="my-3" id="about">
+// //         <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+// //         <p>${aboutText}</p>
+// //       </section>
+// //     `;
+// //   };
+
+// module.exports = generatePage() 
